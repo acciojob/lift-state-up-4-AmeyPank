0fbf52141e67982aeed3e84d2cf9c82fb06a0095
AmeyPank <pankantiameya@gmail.com>
2023-05-24 00:52:20 +0530

    <div className="child">

--- src/components/Parent.js
+++ src/components/Parent.js
@@ -1,80 +1,60 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
+import Child from "./Child";
 
 const Parent = () => {
-  const [cartItems, setCartItems] = useState([]);
+    const [itemName, setItemName] = useState('');
+    const [itemPrice, setItemPrice] = useState('');
+    const [cartItems, setCartItems] = useState([
+        { id: 1, name: 'Item 1', price: 10 },
+        { id: 2, name: 'Item 2', price: 20 },
+        { id: 3, name: 'Item 3', price: 30 }
+    ]);
 
-  const handleAddItem = (itemName, itemPrice) => {
-    const newItem = { id: Date.now(), name: itemName, price: itemPrice };
-    setCartItems([...cartItems, newItem]);
-  };
 
-  const handleRemoveItem = (itemId) => {
-    setCartItems(cartItems.filter(item => item.id !== itemId));
-  };
+    const handleAddItem=()=>{
+        let newItem;
+        if(itemName&&itemPrice){
+            newItem={
+                id: cartItems.length+1,
+                name: itemName,
+                price: Number(itemPrice),
+            }
+        }
+        setCartItems((prevItems)=>[...prevItems,newItem]);
+        setItemName('');
+        setItemPrice('');
+    }
 
-  return (
-    <div className="parent">
-      <h1>Parent Component</h1>
-      <ChildAddItem onAddItem={handleAddItem} />
-      <div className='child-comp'>
-        <h1>Child Component</h1>
-      <ChildCartItems cartItems={cartItems} onRemoveItem={handleRemoveItem} />
-      </div>
-      
-      
-    </div>
-  );
-};
+    const handleRemoveItem = (itemId) => {
 
-const ChildCartItems = ({ cartItems, onRemoveItem }) => {
-  return (
-    <div className="child">
-      {/* <h2>Cart Items</h2> */}
-      <ul>
-        {cartItems.map(item => (
-          <li key={item.id}>
-            <span id="itemName">{item.name}</span>
-            <span id="itemPrice"> - ${item.price}</span>
-            <button onClick={() => onRemoveItem(item.id)}>Remove</button>
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
-};
+        setCartItems((prevItems) => {
+            console.log("this is previtems", prevItems);
+            return prevItems.filter((item) => item.id !== itemId)
 
-const ChildAddItem = ({ onAddItem }) => {
-  const [itemName, setItemName] = useState('');
-  const [itemPrice, setItemPrice] = useState('');
+        })
+    }
 
-  const handleNameChange = (event) => {
-    setItemName(event.target.value);
-  };
+    return (
+        <div className="parent">
 
-  const handlePriceChange = (event) => {
-    setItemPrice(event.target.value);
-  };
+            <h1>Cart</h1>
+            <div className="inputs">
+                <input id="itemName"
+                    type="text" value={itemName} onChange={(e) => setItemName(e.target.value)} placeholder="Item Name" />
+                <input id="itemPrice"
+                    type="number"
+                    value={itemPrice}
+                    onChange={(e) => setItemPrice(e.target.value)}
+                    placeholder="Item Price"
+                />
+                <button onClick={handleAddItem}>Add Item</button>
+            </div>
+            <Child cartItems={cartItems} onRemoveItem={handleRemoveItem}></Child>
 
-  const handleAddClick = () => {
-    onAddItem(itemName, parseFloat(itemPrice));
-    setItemName('');
-    setItemPrice('');
-  };
 
-  return (
-    <div className="child">
-      <h2>Add Item</h2>
-      <div>
-        <label htmlFor="itemName">Item Name:</label>
-        <input type="text" id="itemName" value={itemName} onChange={handleNameChange} />
-      </div>
-      <div>
-        <label htmlFor="itemPrice">Item Price:</label>
-        <input type="number" id="itemPrice" value={itemPrice} onChange={handlePriceChange} />
-      </div>
-      <button onClick={handleAddClick}>Add Item</button>
-    </div>
-  );
-};
+        </div>
+    )
+
+}
 
 export default Parent;
